e123182462cf210292ca45733feff867fad8c9df
Veerash Palanichamy <veerashp05@gmail.com>
2020-01-15 23:52:44 -0500

    added bubblesort

--- sortingalgorithms.h
+++ sortingalgorithms.h
@@ -15,16 +15,16 @@ public:
         a = b;
         b = temp;
     }
-    void SelectionSort(std::vector<T> &arr)
-    {
+
+    void SelectionSort(std::vector<T> &arr){
 
         int len  = arr.size();
         T lowest;
         int lowest_index;
-        for(int i=0;i<len;i++){
+        for(int i=0; i<len; i++){
             lowest  =  arr[i];
             lowest_index = i;
-            for(int j=i;j<len;j++){
+            for(int j=i; j<len; j++){
                 if(*(arr[j])<*lowest){
                     lowest = arr[j];
                     lowest_index = j;
@@ -32,6 +32,22 @@ public:
             }
 
             swap(arr[lowest_index], arr[i]);
+        }
+    }
+
+    void BubbleSort(std::vector<T> &arr){
+        int len = arr.size();
+        bool swapped;
+        for(int i=0;i<len-1;i++){
+            swapped = false;
+            for(int j=0;j<len-1-i;j++){
+                if(*(arr[j+1])<*(arr[j])){
+                    swapped = true;
+                    swap(arr[j], arr[j+1]);
+                }
+            }
+            if(!swapped)
+                break;
         }
     }
 };
